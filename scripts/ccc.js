2a6813abe9a5940c4d85f3b7a7d0fd6951a2f299
Bang1338 <75790567+Bang1338@users.noreply.github.com>
2023-08-16 06:59:57 +0700

    fix some idiotic stuff

--- scripts/ccc.js
+++ scripts/ccc.js
@@ -1,14 +1,11 @@
 // ccc.js, by bang1338
 
+// Hide main content initially
+$('#maincontent').hide();
+
 // Fade in bruh
 $(document).ready(function () {
-
-    // Hide main content initially
-    $('#maincontent').hide();
-
 	audio.play();
-        // Fade out button
-        $(this).fadeOut();
 
         // Show main content
         $('#maincontent').show();
@@ -18,18 +15,4 @@ $(document).ready(function () {
             $(this).delay(i * 500).fadeIn();
             $(this).next('table').delay(i * 500).fadeIn();
         });
-
-		/*
-        // Fade out banner after 70 seconds, but lemme testing first 4 sec
-		// This part is broken
-        setTimeout(function () {
-            $("#banner-1").fadeOut(2000, function () {
-                // After banner-1 completely fade out, slide in from the right banner-2
-                $("#banner-2").css("display", "");
-                //$("#banner-2").css("position", "absolute");
-                $("#banner-2").css("right", "-400px");
-                $("#banner-2").animate({right: "0px"}, 2000);
-            });
-        }, 4000);
-		*/
 });
