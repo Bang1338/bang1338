96a866295e9ac7200b025dce4aaf67a55b4e13e8
Bang1338 <75790567+Bang1338@users.noreply.github.com>
2023-08-15 16:41:24 +0700

    I FORGOR THIS TOO

--- scripts/ccc.js
+++ scripts/ccc.js
@@ -1,15 +1,15 @@
 // ccc.js, by bang1338
 
 // Fade in bruh
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Hide main content initially
     $('#maincontent').hide();
 
     // Add click event to button
-    $('#toggleButton').click(function() {
-		
-	audio.play();
+    $('#toggleButton').click(function () {
+
+        audio.play();
         // Fade out button
         $(this).fadeOut();
 
@@ -17,29 +17,23 @@ $(document).ready(function() {
         $('#maincontent').show();
 
         // Fade in releases and tables one by one
-        $('.releasedate').each(function(i) {
+        $('.releasedate').each(function (i) {
             $(this).delay(i * 500).fadeIn();
             $(this).next('table').delay(i * 500).fadeIn();
         });
-        
-        // Fade out banner after 70 seconds, lemme testing first 4 sec
-        setTimeout(function() {
-            $('#banner').fadeOut(2000, function() {
-                // Replace banner image
-                $('#banner img').attr('src', 'images/fear-factory.png');
 
-                // Wipe in new banner from right to left
-                $('#banner').css({
-                    'width': '400px',
-                    'overflow': 'hidden'
-                });
-                $('#banner img').css({
-                    'position': 'absolute',
-                    'left': '400px'
-                });
-                $('#banner').show();
-                $('#banner img').animate({left: 0}, 2000);
+		/*
+        // Fade out banner after 70 seconds, but lemme testing first 4 sec
+		// This part is broken
+        setTimeout(function () {
+            $("#banner-1").fadeOut(2000, function () {
+                // After banner-1 completely fade out, slide in from the right banner-2
+                $("#banner-2").css("display", "");
+                //$("#banner-2").css("position", "absolute");
+                $("#banner-2").css("right", "-400px");
+                $("#banner-2").animate({right: "0px"}, 2000);
             });
         }, 4000);
+		*/
     });
 });
