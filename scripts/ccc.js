aa698dccaaad822b05a5a2d5268cb8def36033dd
Bang1338 <75790567+Bang1338@users.noreply.github.com>
2023-08-16 07:10:54 +0700

    Update ccc.js

--- scripts/ccc.js
+++ scripts/ccc.js
@@ -3,20 +3,11 @@
 // Hide main content initially
 $('#maincontent').hide();
 
-// Fade in bruh
-$(document).ready(function () {
-	// Add event listener for click 
-	document.body.addEventListener('click', (e) => {
-  		if(e.target === document.body) {
-			// Play audio
-			audio.play();
-			// Show main content
-        		$('#maincontent').show();
-			// Fade in releases and tables one by one
-        		$('.releasedate').each(function (i) {
-            			$(this).delay(i * 500).fadeIn();
-            			$(this).next('table').delay(i * 500).fadeIn();
-        		});
-  		}
-	});
+audio.play();
+// Show main content
+$('#maincontent').show();
+// Fade in releases and tables one by one
+$('.releasedate').each(function (i) {
+        $(this).delay(i * 500).fadeIn();
+        $(this).next('table').delay(i * 500).fadeIn();
 });
